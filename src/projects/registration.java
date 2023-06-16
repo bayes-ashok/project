338a7c4d26886269d255c9789f68a9ba9f933009
prastavana <121224637+prastavana@users.noreply.github.com>
2023-06-16 11:23:04 +0545

    Update registration.java

--- src/projects/registration.java
+++ src/projects/registration.java
@@ -244,7 +244,15 @@ public class registration extends javax.swing.JFrame {
                 i=1;
                 roomNumber.addItem(result.getString(1));
             }
-            
+            if(i==0){
+                JOptionPane.showMessageDialog(this, "All room booked");
+                set();
+            }
+        }
+        catch(Exception e){
+            JOptionPane.showMessageDialog(this, e.getMessage());
+        }
+    }
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
         setVisible(false);
