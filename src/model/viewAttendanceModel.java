97c95974af7b99368f0378378fe5b31cb6a738bd
Ashok <bayes.ashok@gmail.com>
2023-07-19 23:28:58 +0545

    view att

---
+++ src/model/viewAttendanceModel.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.Date;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class viewAttendanceModel {
+    private DefaultTableModel tableData;
+    private Date dateVal;
+    private int rowCount;
+    public viewAttendanceModel(DefaultTableModel tableData, Date dateVal, int rowCount) {
+        this.tableData=tableData;
+        this.dateVal=dateVal;
+        this.rowCount=rowCount;
+    }
+
+    public DefaultTableModel getTableData() {
+        return tableData;
+    }
+
+    public void setTableData(DefaultTableModel tableData) {
+        this.tableData = tableData;
+    }
+
+    public Date getDateVal() {
+        return dateVal;
+    }
+
+    public void setDateVal(Date dateVal) {
+        this.dateVal = dateVal;
+    }
+
+    public int getRowCount() {
+        return rowCount;
+    }
+
+    public void setRowCount(int rowCount) {
+        this.rowCount = rowCount;
+    }
+    
+    
+}
